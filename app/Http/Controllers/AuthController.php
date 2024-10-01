de6b50766d0c3edf0d368ab351505b5f9cb48e6e
Oussa-Err <dexterlecter0@gmail.com>
2024-10-01 18:49:15 +0100

    factory and seeders

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -27,7 +27,7 @@ class AuthController extends Controller
         Auth::login($user);
 
         //Redirect
-        return redirect()->route('home');
+        return redirect()->route('/');
     }
 
 
