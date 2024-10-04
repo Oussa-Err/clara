37d1c88430c3145a37944fa6947730c90c275a5c
Oussa-Err <dexterlecter0@gmail.com>
2024-10-04 19:47:42 +0100

    used reusable postCard instead

--- resources/views/components/postCard.blade.php
+++ resources/views/components/postCard.blade.php
@@ -1,4 +1,4 @@
-@props(['post'])
+@props(['post', 'full'])
 <div class="card">
     <h2 class="font-bold text-xl">
         {{ $post->title }}
@@ -9,10 +9,16 @@
         {{-- route model biding --}}
         <a href={{ route('posts.user', $post->user) }} class="text-blue-500 font-medium">{{ $post->user->username }}</a>
     </div>
-    <div class="text-sm">
-        <span>{{ Str::words($post->body, 15, '...') }}</span>
-        <a href={{ route('post', $post) }} class="text-link">Read more &rarr;</a>
-        {{-- alternatively --}}
-        {{-- <p>{{ substr($post->body, 0, 100 )}}...</p> --}}
-    </div>
+    @if ('full')
+        <div class="text-sm">
+            <span>{{ $post->body }}</span>
+        </div>
+    @else
+        <div class="text-sm">
+            <span>{{ Str::words($post->body, 15, '...') }}</span>
+            <a href={{ route('post', $post) }} class="text-link">Read more &rarr;</a>
+            {{-- alternatively --}}
+            {{-- <p>{{ substr($post->body, 0, 100 )}}...</p> --}}
+        </div>
+    @endif
 </div>
--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -1,16 +1,4 @@
 <x-layout>
     <h1 class="title">View post</h1>
-    <div class="card">
-        <h2 class="font-bold text-xl">
-            {{ $post->title }}
-        </h2>
-        <div class="text-xs font-light mb-4">
-            <span>Posted {{ $post->created_at->diffForHumans() }} BY</span>
-            <a href={{ route('posts.user', $post->user) }}
-                class="text-blue-500 font-medium">{{ $post->user->username }}</a>
-        </div>
-        <div class="text-sm">
-            <p>{{ $post->body }}</p>
-        </div>
-    </div>
+    <x-postCard :post=$post full/>
 </x-layout>
